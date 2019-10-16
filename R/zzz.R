6a2a9084dff64cb8fd06a52fd237b6c7a0a43651
Miguel A. Sorrel <miguel.sorrel@uam.es>
2019-10-16 15:48:21 +0200

    Remove

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onLoad <- function(...) {
-  packageStartupMessage("cdcatR Package [Version 0.9.2; 2019-10-16]
-                        More information: https://github.com/miguel-sorrel/cdcatR")
-}
